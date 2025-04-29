81dcd75293cd5c46e60fd3ce83f66dd1bde5167f
eekon <eekon@qq.com>
2025-04-29 11:01:11 +0800

    修改替换API地址

--- src/common.rs
+++ src/common.rs
@@ -934,7 +934,7 @@ fn get_api_server_(api: String, custom: String) -> String {
             return format!("http://{}", s);
         }
     }
-    "http://home.mujj.top".to_owned()
+    "http://home.mujj.top:21114".to_owned()
 }
 
 pub fn get_audit_server(api: String, custom: String, typ: String) -> String {
